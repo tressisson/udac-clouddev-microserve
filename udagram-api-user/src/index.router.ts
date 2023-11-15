80a0a3e13eae2936a7eed539e5992a34559fb106
Tres Sisson <tressisson@gmail.com>
2023-11-15 08:58:27 -0500

    split feed and user code

--- udagram-api-user/src/index.router.ts
+++ udagram-api-user/src/index.router.ts
@@ -1,10 +1,8 @@
 import {Router, Request, Response} from 'express';
-import {FeedRouter} from './src/routes/feed.router';
 import {UserRouter} from '../udagram-api-user/src/routes/user.router';
 
 const router: Router = Router();
 
-router.use('/feed', FeedRouter);
 router.use('/users', UserRouter);
 
 router.get('/', async (req: Request, res: Response) => {
--- udagram-api-user/src/model.index.ts
+++ udagram-api-user/src/model.index.ts
@@ -1,6 +1,3 @@
-import {FeedItem} from './src/models/FeedItem';
 import {User} from '../udagram-api-user/src/models/User';
 
-
 export const V0_USER_MODELS = [User];
-export const V0_FEED_MODELS = [FeedItem];
