2f1f9f61df08649f61a55ad1b12e551aa42e7b25
Tres Sisson <tressisson@gmail.com>
2023-11-15 08:20:24 -0500

    starter ft and back working?

--- udagram-api/src/sequelize.ts
+++ udagram-api/src/sequelize.ts
@@ -1,5 +1,6 @@
 import { Sequelize } from "sequelize-typescript";
 import { config } from "./config/config";
+
 export const sequelize = new Sequelize({
   username: config.username,
   password: config.password,
@@ -9,7 +10,7 @@ export const sequelize = new Sequelize({
   storage: ":memory:",
   dialectOptions: {
     ssl: {
-      require: true,
+      require: false,
       rejectUnauthorized: false, // added for compatibility with RDS
     },
   },
